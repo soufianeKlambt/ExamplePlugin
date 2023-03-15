e80894ee228d6434f752e1f76442b7a0a4a0b1ca
soufianeKlambt <127092534+soufianeKlambt@users.noreply.github.com>
2023-03-15 14:32:58 +0100

    Update MyExampleWidget.php

--- Widgets/MyExampleWidget.php
+++ Widgets/MyExampleWidget.php
@@ -74,13 +74,11 @@ class MyExampleWidget extends Widget
 
               $db = \Piwik\Db::get();
         $data=array();
-        $rows = $db->fetchAll('select * from matomo_log_link_visit_action where idsite = 1 limit 5');
-foreach ($rows as $row) {
-    $data[]=$row['idvisit'];
-} 
-        print_r($data);
+                $idSite   = self::$fixture->idSite;
+        $rows = $db->fetchAll("SELECT concat('https://'pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = 4 ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
+
        return $this->renderTemplate('myViewTemplate', array(
-            'data' => $data,
+            'data' => $rows,
         ));
 
        
