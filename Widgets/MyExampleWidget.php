0cdf2e3114a5230abb851f15bf1212de6f1f46f6
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-15 15:09:02 +0100

    add idsite

--- Widgets/MyExampleWidget.php
+++ Widgets/MyExampleWidget.php
@@ -10,58 +10,17 @@ namespace Piwik\Plugins\ExamplePlugin\Widgets;
 
 use Piwik\Widget\Widget;
 use Piwik\Widget\WidgetConfig;
-use Piwik\Db\SchemaInterface;
-use Piwik\Tests\Fixtures\TwoSitesEcommerceOrderWithItems;
-use Piwik\Db;
-/**
- * This class allows you to add your own widget to the Piwik platform. In case you want to remove widgets from another
- * plugin please have a look at the "configureWidgetsList()" method.
- * To configure a widget simply call the corresponding methods as described in the API-Reference:
- * http://developer.piwik.org/api-reference/Piwik/Plugin\Widget
- */
+
+
 class MyExampleWidget extends Widget
 {
         public static $fixture = null; // initialized below class definition
 
     public static function configure(WidgetConfig $config)
     {
-        /**
-         * Set the category the widget belongs to. You can reuse any existing widget category or define
-         * your own category.
-         */
         $config->setCategoryId('About Matomo');
-
-        /**
-         * Set the subcategory the widget belongs to. If a subcategory is set, the widget will be shown in the UI.
-         */
-        // $config->setSubcategoryId('General_Overview');
-
-        /**
-         * Set the name of the widget belongs to.
-         */
         $config->setName('Example Widget Name');
-
-        /**
-         * Set the order of the widget. The lower the number, the earlier the widget will be listed within a category.
-         */
         $config->setOrder(99);
-
-        /**
-         * Optionally set URL parameters that will be used when this widget is requested.
-         * $config->setParameters(array('myparam' => 'myvalue'));
-         */
-
-        /**
-         * Define whether a widget is enabled or not. For instance some widgets might not be available to every user or
-         * might depend on a setting (such as Ecommerce) of a site. In such a case you can perform any checks and then
-         * set `true` or `false`. If your widget is only available to users having super user access you can do the
-         * following:
-         *
-         * $config->setIsEnabled(\Piwik\Piwik::hasUserSuperUserAccess());
-         * or
-         * if (!\Piwik\Piwik::hasUserSuperUserAccess())
-         *     $config->disable();
-         */
     }
 
     /**
@@ -74,11 +33,9 @@ class MyExampleWidget extends Widget
      */
     public function render()
     {
-   echo $_GET['idSite']."yyy";
-              $db = \Piwik\Db::get();
-        $data=array();
-       $rows = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = 4 ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
-
+       $idSite=$_GET['idSite'];
+        $db = \Piwik\Db::get();
+       $rows = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = ".$idSite." ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
        return $this->renderTemplate('myViewTemplate', array(
             'rows' => $rows,
         ));
