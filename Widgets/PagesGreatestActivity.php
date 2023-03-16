08ca1c55e29f1974e32164f26b11b627043c40e4
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-16 12:28:25 +0100

    cache remove function

--- Widgets/PagesGreatestActivity.php
+++ Widgets/PagesGreatestActivity.php
@@ -33,7 +33,7 @@ class PagesGreatestActivity extends Widget
     public function render()
     {
       $file= "/var/www/html/plugins/PagesGreatestActivity/Widgets/data.cache";
-      $expire = 900;
+      $expire = 9;
       if (filemtime($file) < (time() - $expire)) {
         $db = \Piwik\Db::get();
         $idSite = $_GET['idSite'];
