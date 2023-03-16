a93a334a43d1a709bad8d8a00b29fc8c18ca2c46
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-16 12:11:42 +0100

    cache added

--- Widgets/PagesGreatestActivity.php
+++ Widgets/PagesGreatestActivity.php
@@ -14,7 +14,8 @@ use Piwik\Widget\WidgetConfig;
 
 class PagesGreatestActivity extends Widget
 {
-        public static $fixture = null; // initialized below class definition
+    public static $file= dirname(__FILE__).'/data.cache';
+    public static $expire = 900;
 
     public static function configure(WidgetConfig $config)
     {
@@ -24,7 +25,6 @@ class PagesGreatestActivity extends Widget
     }
 
     /**
-     * https://gist.github.com/johnvilsack/cfe7a36c0582d4ebf8cc
      * This method renders the widget. It's on you how to generate the content of the widget.
      * As long as you return a string everything is fine. You can use for instance a "Piwik\View" to render a
      * twig template. In such a case don't forget to create a twig template (eg. myViewTemplate.twig) in the
@@ -35,22 +35,36 @@ class PagesGreatestActivity extends Widget
     public function render()
     {
        $idSite=$_GET['idSite'];
-            $file= dirname(__FILE__).'/data.cache';
-       $db = \Piwik\Db::get();
-       $rows = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = ".$idSite." ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
-      
-            $result = json_encode($rows);
-
-	// Put data in file.
-	$fp = fopen($file,"w");
-	fputs($fp,$result);
-	fclose($fp);
             
-            return $this->renderTemplate('myViewTemplate', array(
-            'rows' => $rows,
+        return $this->renderTemplate('myViewTemplate', array(
+            'rows' => $file_exists($this->file) ? $this->checkCache($this->file) : $this->makeCache($this->file),
         ));
 
        
     }
+
+    static function makeCache($file) {
+
+        $db = \Piwik\Db::get();
+        $result = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = ".$idSite." ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
+        $fp = fopen($file,"w");
+        fputs($fp,$result);
+        fclose($fp);
+        return $result;
+    
+    }
+    
+    // Function to check cache and retrieve its bits.
+    static function checkCache($file) {
+    
+        if (filemtime($file) < (time() - $this->expire)) {
+            $result = makeCache($file);
+        } else {
+            $result = file_get_contents($file);
+        }
+    
+        return $result;
+    }
+    
 
 }
