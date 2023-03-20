0ab712521c1744624ac342e294a15f4541039bfb
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-20 16:07:58 +0100

    undo change

--- Widgets/PageViews.php
+++ Widgets/PageViews.php
@@ -31,9 +31,28 @@ class PageViews extends Widget {
    * @return string
    */
   public function render() {
-
+    $idSite = $_GET['idSite'];
+    $file = '/var/www/html/cache/WidgetKLAMBT-referer-site-' . $idSite . '.cache';
+    $expire = 300;
+    if (filemtime($file) < (time() - $expire)) {
+      $db = \Piwik\Db::get();
+      $result = $db->fetchAll("SELECT TIMESTAMPDIFF(Minute,UTC_TIMESTAMP(),matomo_log_link_visit_action.server_time) as timeinterval, count(matomo_log_action.name) as hits FROM matomo_log_link_visit_action INNER JOIN matomo_log_action ON matomo_log_link_visit_action.idaction_url = matomo_log_action.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 31 MINUTE)) AND matomo_log_link_visit_action.idsite = ".$idSite." GROUP BY timeinterval ORDER BY timeinterval desc LIMIT 0,30");
+      $fp = fopen($file, "w");
+      fputs($fp, json_encode($result));
+      fclose($fp);
+    }
+    else {
+      $result =json_decode( file_get_contents($file),true);
+    }
+    $xValues= array();
+    $yValues= array();
+    foreach($result as $values) {
+      $xValues[]=$values['timeinterval'];
+      $yValues[]=$values['hits'];
+    }
     return $this->renderTemplate('pageviewsTemplate', [
-
+      'xValues' => $xValues,
+      'yValues' => $yValues,
     ]);
   }
 
