c763b7768fa7af4d8595edade3995ee3638a5d8b
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-21 16:04:29 +0100

    -

--- GetSimpleLastVisitCount.php
+++ GetSimpleLastVisitCount.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
  */
-namespace Piwik\Plugins\Live\Reports;
+namespace Piwik\Plugins\WidgetKLAMBT\Widgets;
 
 use Piwik\Config;
 use Piwik\Metrics\Formatter;
