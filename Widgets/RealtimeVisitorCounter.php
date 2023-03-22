20c88c324077825013d0a73f004de88787d84782
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-22 11:50:08 +0100

    -

---
+++ Widgets/RealtimeVisitorCounter.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\WidgetKLAMBT\Widgets;
+
+use Piwik\Widget\Widget;
+use Piwik\Widget\WidgetConfig;
+
+
+class RealtimeVisitorCounter extends Widget {
+
+  public static function configure(WidgetConfig $config) {
+    $config->setCategoryId('Besucher');
+    $config->setName('Echtzeit Aktive Besucher');
+    $config->setOrder(96);
+  }
+
+  /**
+   * This method renders the widget. It's on you how to generate the content of
+   * the widget. As long as you return a string everything is fine. You can use
+   * for instance a "Piwik\View" to render a twig template. In such a case
+   * don't forget to create a twig template (eg. myViewTemplate.twig) in the
+   * "templates" directory of your plugin.
+   *
+   * @return string
+   */
+  public function render() {
+    $idSite = $_GET['idSite'];
+  echo "KLAMBT";
+
+
+  }
+
+
+}
