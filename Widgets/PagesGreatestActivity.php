a7fba9f531265ef9e7d952f51eae0015f5e908cd
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-24 09:10:07 +0100

    decode cache

--- Widgets/PagesGreatestActivity.php
+++ Widgets/PagesGreatestActivity.php
@@ -37,14 +37,15 @@ class PagesGreatestActivity extends Widget {
     $idSite = $_GET['idSite'];
     $cacheKey = 'WidgetKLAMBT-PGA-site-' . $idSite;
     $expire = 300;
-    $result = $this->cache->fetch($cacheKey);
+    $result = json_decode($this->cache->fetch($cacheKey));
+    print_r($result);
     if ($result === false) {
       $db = \Piwik\Db::get();
       $result = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = " . $idSite . " ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
-      $this->cache->save($cacheKey, $result, $expire);
+      $this->cache->save($cacheKey, json_encode($result), $expire);
     }
     $sum = 0;
-    foreach($result as $num => $values) {
+    foreach($result as $values) {
       $sum += $values[ 'visits' ];
     }
     return $this->renderTemplate('PGATemplate', [
