abd70369654f4dde56dbccb4a5244dfbf09d60d9
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-24 09:07:04 +0100

    add cache matomo

--- Widgets/PagesGreatestActivity.php
+++ Widgets/PagesGreatestActivity.php
@@ -14,7 +14,10 @@ use Piwik\Widget\WidgetConfig;
 
 
 class PagesGreatestActivity extends Widget {
-
+  /**
+   * @var Lazy
+   */
+  private Lazy $cache;
   public static function configure(WidgetConfig $config) {
     $config->setCategoryId('Besucher');
     $config->setName('Seiten mit größter Aktivität (30 Min)');
@@ -32,17 +35,13 @@ class PagesGreatestActivity extends Widget {
    */
   public function render() {
     $idSite = $_GET['idSite'];
-    $file = '/var/www/html/cache/WidgetKLAMBT-PGA-site-' . $idSite . '.cache';
+    $cacheKey = 'WidgetKLAMBT-PGA-site-' . $idSite;
     $expire = 300;
-    if (filemtime($file) < (time() - $expire)) {
+    $result = $this->cache->fetch($cacheKey);
+    if ($result === false) {
       $db = \Piwik\Db::get();
       $result = $db->fetchAll("SELECT concat('https://',pageimpressions.url) as full_url,REGEXP_REPLACE(pageimpressions.url,'^[a-zA-Z0-9\.\-]*','') as relative_url,pageimpressions.idsite,COUNT(pageimpressions.idvisit) as visits FROM (SELECT matomo_log_link_visit_action.server_time,REGEXP_REPLACE(action_url.name,'[\?|#].*$', '') as url,matomo_log_link_visit_action.idsite,matomo_log_link_visit_action.idvisit,matomo_log_link_visit_action.idpageview,matomo_log_link_visit_action.idaction_url,action_url.hash,action_url.type FROM matomo_log_link_visit_action INNER JOIN matomo_log_action as action_url ON matomo_log_link_visit_action.idaction_url = action_url.idaction WHERE matomo_log_link_visit_action.server_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 30 MINUTE))  AND idsite = " . $idSite . " ORDER BY matomo_log_link_visit_action.idlink_va desc) as pageimpressions GROUP BY `url`, idsite ORDER BY visits desc LIMIT 40");
-      $fp = fopen($file, "w");
-      fputs($fp, json_encode($result));
-      fclose($fp);
-    }
-    else {
-      $result =json_decode( file_get_contents($file),true);
+      $this->cache->save($cacheKey, $result, $expire);
     }
     $sum = 0;
     foreach($result as $num => $values) {
