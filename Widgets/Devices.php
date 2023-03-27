1425c2f51bb7969e883e67668bba62d57f69ed40
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-27 12:29:44 +0200

    -

--- Widgets/Devices.php
+++ Widgets/Devices.php
@@ -33,7 +33,7 @@ class Devices extends Widget {
    */
   public function render() {
     $idSite = $_GET['idSite'];
-    $sql = "SELECT CASE WHEN config_device_type = 1  THEN 'Smartphone' WHEN config_device_type = 0  THEN 'Tablet' WHEN config_device_type = 10 THEN 'Tablet' WHEN config_device_type = 2  THEN 'Desktop/Notebook' ELSE 'andere' END as devices, count(idvisit) as visits FROM matomo_log_visit WHERE idsite = ".$idSite." AND visit_last_action_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 5 MINUTE)) GROUP BY devices ORDER BY visits desc LIMIT 10";
+    $sql = "SELECT CASE WHEN config_device_type = 1  THEN 'Mobilgerät' WHEN config_device_type = 0  THEN 'Tablet' WHEN config_device_type = 10 THEN 'Tablet' WHEN config_device_type = 2  THEN 'Computer' ELSE 'andere' END as devices, count(idvisit) as visits FROM matomo_log_visit WHERE idsite = ".$idSite." AND visit_last_action_time >= (DATE_SUB(UTC_TIMESTAMP(),INTERVAL 5 MINUTE)) GROUP BY devices ORDER BY visits desc LIMIT 10";
     $cache=new WKCache();
     $result= $cache->getCacheData('Devices-'.$idSite,$sql);
     $sum = 0;
