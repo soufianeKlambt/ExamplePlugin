94d0e4f8c9304432affb8abd9e3559cf22c449da
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-31 15:08:15 +0200

    -

--- js/refresh.js
+++ js/refresh.js
@@ -1,4 +1,4 @@
 setInterval(function (){
   console.log('refreshed');
-  $('#widgetWidgetKLAMBTdevices').dashboardWidget('reload', false, true);
+  $('[widgetid=widgetWidgetKLAMBTdevices]').dashboardWidget('reload', false, true);
 }, 1800);
