e3231174bf0fca9e25c346793641dad64124c1dd
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-03-31 16:39:42 +0200

    -

--- Widgets/KlambtEchtzeit.php
+++ Widgets/KlambtEchtzeit.php
@@ -21,6 +21,7 @@ class KlambtEchtzeit extends \Piwik\Widget\Widget
   {
     $config->setCategoryId('Echtzeit');
     $config->setSubcategoryId('Besucher');
+    $config->setId()('KlambtEchtzeit');
   }
   public function render()
   {
