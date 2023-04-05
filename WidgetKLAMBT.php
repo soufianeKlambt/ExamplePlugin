a8b540b0142efa47b9fa4550d12cc04552ed6a80
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-04-05 11:01:04 +0200

    -

--- WidgetKLAMBT.php
+++ WidgetKLAMBT.php
@@ -28,10 +28,6 @@ class WidgetKLAMBT extends \Piwik\Plugin
   public function getJsFiles(&$jsFiles)
   {
     $jsFiles[] = 'plugins/WidgetKLAMBT/js/Chart.js';
-    $jsFiles[] = 'plugins/Dashboard/javascripts/dashboard.js';
-    $jsFiles[] = 'plugins/Dashboard/javascripts/dashboardObject.js';
-    $jsFiles[] = 'plugins/Dashboard/javascripts/dashboardWidget.js';
-    $jsFiles[] = 'plugins/WidgetKLAMBT/js/refresh.js';
   }
 
     // support archiving just this plugin via core:archive
