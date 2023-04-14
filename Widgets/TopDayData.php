cac0b3d4a084ca0b97eb7be532095b50cd3900cd
soufiane.belkouchi <soufiane.belkouchi@klambt.de>
2023-04-14 15:05:36 +0200

    -

---
+++ Widgets/TopDayData.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\WidgetKLAMBT\Widgets;
+
+use Piwik\Plugins\WidgetKLAMBT\WKCache;
+use Piwik\Widget\Widget;
+use Piwik\Widget\WidgetConfig;
+
+
+class TopDayData extends Widget {
+
+
+
+  /**
+   * This method renders the widget. It's on you how to generate the content of
+   * the widget. As long as you return a string everything is fine. You can use
+   * for instance a "Piwik\View" to render a twig template. In such a case
+   * don't forget to create a twig template (eg. myViewTemplate.twig) in the
+   * "templates" directory of your plugin.
+   *
+   * @return string
+   */
+  public function render() {
+    $idSite = $_GET['idSite'];
+    $date = $_GET['date'];
+    $sql = "select id,datum,url,pageimpressions from klambt_day_data where site_id=".$idSite." and datum = '".$date."' group by url order by pageimpressions desc limit 20";
+    $cache=new WKCache();
+    $result= $cache->getCacheData('TopDayData-'.$idSite,$sql);
+   return json_encode($result);
+
+  }
+
+
+}
